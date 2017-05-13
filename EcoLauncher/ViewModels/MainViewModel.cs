fdd161b8ac781b4d3f963bec067d25606bd8d8a6
ShTair <a@vsh.jp>
2017-05-13 13:19:40 +0900

    viewの大枠

---
+++ EcoLauncher/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace EcoLauncher.ViewModels
+{
+    class MainViewModel :INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Status
+        {
+            get { return _Status; }
+            set
+            {
+                if (_Status == value) return;
+                _Status = value;
+                PropertyChanged?.Invoke(this, _StatusChangedEventArgs);
+            }
+        }
+        private string _Status;
+        private PropertyChangedEventArgs _StatusChangedEventArgs = new PropertyChangedEventArgs(nameof(Status));
+
+        public MainViewModel()
+        {
+            Status = "準備完了";
+        }
+    }
+}
